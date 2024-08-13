3aa4990e7fd76927eba0710e585ca87566e983d2
Hash-itPatel <Jmharshitpatel@gmail.com>
2024-08-13 12:28:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -228,9 +228,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
-
-<b> Size ⚙️: {file_size}</b>""" 
+    CAPTION = """<a href='https://t.me/Ipapkornelite'>{file_name}</a>""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
