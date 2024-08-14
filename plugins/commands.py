4be9f9f3a6729b8b7812b6e4910fd1c06f72a6ca
Hash-itPatel <Jmharshitpatel@gmail.com>
2024-08-14 19:21:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 join_db = JoinReqs
-REACTION = [ 🙂, 😁,🥰,🤯,😈,👻,😎,🤩,🥳,🤗,😠]
+REACTION = [ "🙂", "😁","🥰","🤯","😈","👻","😎","🤩","🥳","🤗","😠"]
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     await message.react(emoji=random.choice(REACTION), big=True)
